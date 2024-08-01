20679233d8d379a859a289868ccdef938f1cd41c
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2024-08-01 15:47:26 +0545

    added add to product

--- frontend/src/Components/ProductPage.jsx
+++ frontend/src/Components/ProductPage.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductPage.css';
 import { useCart } from '../context/CartContext';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { FetchProductWithId } from '../service/api';
-import { Link } from 'react-router-dom';
 
 const ProductPage = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState("");
+  const [product, setProduct] = useState(null);
+  const { dispatch } = useCart();
 
   useEffect(() => {
     // Fetch product details from your API
@@ -24,17 +24,8 @@ const ProductPage = () => {
     fetchProduct();
   }, [id]);
 
-  const addToCart = () => {
-    const data = {
-      userId: id,
-      items: [
-        {
-          productId: id,
-          quantity: 1
-        }
-      ]
-    };
-    console.log(data);
+  const addToCart = (product) => {
+    dispatch({ type: 'ADD_TO_CART', payload: product });
   };
 
   if (!product) {
@@ -47,16 +38,14 @@ const ProductPage = () => {
         <img src={`../../public/${product.imageUrl}`} alt={product.name} className="product-image" />
       </div>
       <div className="product-details">
-      
         <h2 className="product-name">{product.name}</h2>
         <p className="product-price"><strong>Price:</strong> Rs.{product.price}</p>
         <p className='product-frame'><strong>Frame Material:</strong> {product.frame_material}</p>
         <p className='product-lens'><strong>Lens Material:</strong> {product.lens_material}</p>
         <p className='product-shape'><strong>Frame Shape:</strong> {product.frame_shape}</p>
-
         <p className="product-description"><strong>Description:</strong> {product.description}</p>
         <p className="product-category"><strong>Category:</strong> {product.category}</p>
-        <Link to={`/cart`}>
+        <Link to="/cart">
           <button onClick={() => addToCart(product)} className="add-to-cart-button">
             Add to Cart
           </button>
