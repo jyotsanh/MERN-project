c011fe38ef8229309520ae0b3f1aba8f3777fe8c
Jyotsan HAMAL <hamaljyotsan@gmail.com>
2024-08-22 10:34:22 +0545

    login changes

--- frontend/src/pages/Admin/adminLogIn.jsx
+++ frontend/src/pages/Admin/adminLogIn.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { AdminLogin } from '../../service/api';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import './adminlogin.css';
+import './adminLogin.css';
 
 function AdminLogIn() {
     const navigate = useNavigate();
