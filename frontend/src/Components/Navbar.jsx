46cbf416d3873c8f9a587f4f1c13dac0c3eb3017
Jyotsan HAMAL <hamaljyotsan@gmail.com>
2024-10-03 21:56:22 +0545

    order navbar updated

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -111,6 +111,7 @@ function Navbar() {
         <li><NavLink to="/book" activeClassName="active">Book Appointment</NavLink></li>
         <li><NavLink to="/faq" activeClassName="active">FAQs</NavLink></li>
         <li><NavLink to="/admin-login" activeClassName="active">Admin</NavLink></li>
+        <li><NavLink to="/myorders" activeClassName="active">My Orders</NavLink></li>
       </ul>
     </div>
   );
