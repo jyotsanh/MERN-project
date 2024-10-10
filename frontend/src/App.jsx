4b9d163a8b39749ed368d1436e05ad912c0a0d8a
lavshah19 <lavshah51@gmail.com>
2024-10-10 05:53:16 -0500

    new feature added with extra efficiency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './pages/auth/AuthContext';
 import Home from './pages/home/Home';
+// dsfjbksjbdjdshbc
 import Order from './pages/order/Order';
 import AllOrders from './pages/AllOrders/AllOrders';
 import Navbar from './Components/Navbar';
